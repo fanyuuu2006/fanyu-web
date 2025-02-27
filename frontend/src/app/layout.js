7e517c9409964e3227ffc908ed2f99cdd87a8c8c
Yorn90104 <bingxiao526@gmail.com>
2025-02-27 23:20:10 +0800

    feat: 設置網展主體預設邊距

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -4,16 +4,16 @@ import Navbar from "@/components/NavBar";
 export const metadata = {
   title: "飯魚 FanYu",
   description: "不是一般人的一般人",
-  icons: "/CampPhoto.ico"
+  icons: "/CampPhoto.ico",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="zh-TW">
-      <body >
-        <Navbar/>
-        {children}
-      </body>
+      <header>
+        <Navbar />
+      </header>
+      <body>{children}</body>
     </html>
   );
 }
