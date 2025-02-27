83d6de84e965fd38091e753644f2b76adfcea2c5
Yorn90104 <bingxiao526@gmail.com>
2025-02-28 00:04:08 +0800

    feat: 導覽列加入 LOGO

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,22 @@
 "use client";
-import "@/styles/Navbar.css"
+import "@/styles/Navbar.css";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Logo from "@/assets/images/Logo.png";
 
 export default function Navbar() {
- return(
-    <header></header>
+  const router = useRouter();
+
+  return (
+    <header>
+      <Image
+        priority
+        src={Logo}
+        alt="Logo"
+        onClick={() => {
+          router.push("/");
+        }}
+      />
+    </header>
   );
 }
